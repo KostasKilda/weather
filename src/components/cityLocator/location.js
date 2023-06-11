f975f942152ed4d3566318aafa95d73d3842a1fe
KostasKilda <kostas.kilda@gmail.com>
2023-06-11 20:39:25 +0100

    Added json extraction

--- src/components/cityLocator/location.js
+++ src/components/cityLocator/location.js
@@ -7,6 +7,7 @@ function IP () {
             .then(response => response.json())
             .then(data => {
                 fetch(`https://geolocation-db.com/json/548bd320-00be-11ee-82dd-87424d907439/${data.ip}`)
+                .then(response => response.json())
                 .then(location =>{
                     console.log(location)
                 })
