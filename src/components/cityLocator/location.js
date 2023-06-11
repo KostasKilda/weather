9af9eba8acfd53b391c2a7964902f70f8c1b8f57
KostasKilda <kostas.kilda@gmail.com>
2023-06-12 00:06:22 +0100

    Obtained city name, wind speed, temperature using API

--- src/components/cityLocator/location.js
+++ src/components/cityLocator/location.js
@@ -1,24 +1,63 @@
 import React from 'react'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-function IP () {
-    useEffect(() => {
-        fetch('https://api.ipify.org/?format=json')
-            .then(response => response.json())
-            .then(data => {
-                fetch(`https://geolocation-db.com/json/548bd320-00be-11ee-82dd-87424d907439/${data.ip}`)
-                .then(response => response.json())
-                .then(location =>{
-                    console.log(location)
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-            })
-            .catch(error => console.error(error));
-    }, []);
+function IP() {
 
-    return null;
+  const [wind, windSet] = useState(0);
+  const [temperature, temperatureSet] = useState(0);
+  const [city, citySet] = useState(0);
+
+  let weather = {
+    temperature: 0,
+    overall: null,
+    wind: 0,
+  }
+
+  useEffect(() => {
+
+
+    let apiKey = '71bf921d4b7b00af38a08d679c35ea3b';
+
+    async function getLocation() {
+      try {
+        const ipResponse = await fetch('https://api.ipify.org/?format=json');
+        const data = await ipResponse.json();
+
+        const locationResponse = await fetch(`http://ip-api.com/json/${data.ip}`);
+        const locationData = await locationResponse.json();
+        citySet(locationData.city);
+
+        const apiKey = '71bf921d4b7b00af38a08d679c35ea3b';
+        const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${locationData.lat}&lon=${locationData.lon}&units=metric&appid=${apiKey}`);
+        const weatherPayload = await weatherResponse.json();
+
+        weather.temperature = weatherPayload.main.temp;
+        weather.overall = weatherPayload.weather[0].main;
+        weather.wind = weatherPayload.wind.speed;
+
+        console.log(weather)
+
+        temperatureSet(weather.temperature)
+        windSet(weather.wind)
+
+        console.log('----------------------------------\n\n')
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+
+    getLocation();
+
+  }, []);
+
+  return (
+    <div>
+      <div>City: <b>{city}</b></div>
+      <div>Current temperature: {temperature}c°</div>
+      <div>Wind speed: {wind}m/s</div>
+    </div>
+  )
 }
 
 export default IP;
