f0ee96c52d07b14867683c0b43be0a6b93ef7e16
KostasKilda <kostas.kilda@gmail.com>
2023-06-13 12:51:23 +0100

    Added temperature logo differences and rain effect

--- src/components/cityLocator/location.js
+++ src/components/cityLocator/location.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './style.css';
 import windLogo from '../../images/windSpeed.png'
 import tempLogo from '../../images/temp.png'
+import hotTempLogo from '../../images/hotTemp.png'
+import coldTempLogo from '../../images/coldTemp.png'
+
 import { useEffect, useState } from 'react';
 
 function IP() {
@@ -20,10 +23,40 @@ function IP() {
   // possible {overall} conditions:
   let weatherConditions = ['Mist', 'Clouds', 'Clear', 'Rain']
 
+
+  function makeItRain() {
+    console.log('Making it rain')
+    let increment = 0;
+    let drops = "";
+    let backDrops = "";
+
+    while (increment < 100) {
+      let randoHundo = Math.floor(Math.random() * (98 - 1 + 1) + 1);
+      let randoFiver = Math.floor(Math.random() * (5 - 2 + 1) + 2);
+      increment += randoFiver;
+
+      drops += `<div class="drop" style="left: ${increment}%; bottom: ${randoFiver + randoFiver - 1 + 100
+        }%; animation-delay: 0.${randoHundo}s; animation-duration: 0.5${randoHundo}s;">
+            <div class="stem" style="animation-delay: 0.${randoHundo}s; animation-duration: 0.5${randoHundo}s;"></div>
+            <div class="splat" style="animation-delay: 0.${randoHundo}s; animation-duration: 0.5${randoHundo}s;"></div>
+          </div>`;
+
+      backDrops += `<div class="drop" style="right: ${increment}%; bottom: ${randoFiver + randoFiver - 1 + 100
+        }%; animation-delay: 0.${randoHundo}s; animation-duration: 0.5${randoHundo}s;">
+            <div class="stem" style="animation-delay: 0.${randoHundo}s; animation-duration: 0.5${randoHundo}s;"></div>
+            <div class="splat" style="animation-delay: 0.${randoHundo}s; animation-duration: 0.5${randoHundo}s;"></div>
+          </div>`;
+    }
+
+    const rainFrontRow = document.querySelector('.rain.front-row');
+    rainFrontRow.innerHTML = drops;
+  }
+
+
+
+
+
   useEffect(() => {
-
-
-
     async function getLocation() {
       try {
         const locationAPIKey = '3e2afa18d85c4564a12ca3c8ac7f8300'
@@ -43,6 +76,12 @@ function IP() {
         temperatureSet(weather.temperature)
         windSet(weather.wind)
         overallSet(weather.overall)
+
+        if (weather.overall == 'Clear') {
+          makeItRain();
+        }
+
+
       } catch (error) {
         console.error(error);
       }
@@ -56,6 +95,7 @@ function IP() {
   return (
     <div className='weatherWindow'>
       <div>
+        <div className="rain front-row"></div>
         <div className='weatherAnimation'>
           <div>Weather outside: {overall}
           </div>
@@ -72,7 +112,13 @@ function IP() {
                   <div className='info'>
                     {temperature}c°
                   </div>
-                  <img src={tempLogo} alt="Logo" />
+                  {temperature >= 20 ? (
+                    <img src={hotTempLogo} alt="Hot temperature logo" />
+                  ) : temperature <= 0 ? (
+                    <img src={coldTempLogo} alt="Cold temperature logo" />
+                  ) : (
+                    <img src={tempLogo} alt="Normal temperature logo" />
+                  )}
                 </div>
               </div>
             </div>
